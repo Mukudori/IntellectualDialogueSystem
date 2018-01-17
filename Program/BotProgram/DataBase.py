c45c82fa0d85f979ce5dd66b29c5aa406fdb37ab
Mukudori <vuramu@outlook.com>
2018-01-17 18:48:42 +0700

    Подключение к бд без использования PySql

--- Program/BotProgram/DataBase.py
+++ Program/BotProgram/DataBase.py
@@ -1,10 +1,12 @@
 import MySQLdb
+from PyQt5.QtGui import QStandardItem, QStandardItemModel
+from PyQt5 import QtCore
 
 #Данные по подключению к БД
 connStr = '127.0.0.1;root;4862159357;botdb;utf8'
 
 #Выполнить запрос и вернуть кортеж из словарей
-def GetTable(sql):
+def GetData(sql):
     cs = connStr.split(';')
     try:
         conn = MySQLdb.connect(host=cs[0], user=cs[1], passwd=cs[2], db=cs[3])
@@ -22,3 +24,34 @@ def GetTable(sql):
     except MySQLdb.Error as err:
         print("Query error: {}".format(err))
     return data
+
+def GetTableViewModel(sql, table='tab'):
+    data  = GetData(sql)
+    model = QStandardItemModel()
+    horhead = list(data[0].keys())
+    model.setHorizontalHeaderLabels(horhead)
+
+    for i in range(len(data)):
+        for j in range(len(horhead)):
+            item = QStandardItemModel()
+            if table == 'dialogtab' and (horhead[j] == 'question' or horhead[j] == 'answer'):
+                item = QStandardItem(GetTextFromCodes(data[i][horhead[j]]))
+            else:
+                item = QStandardItem(str(data[i][str(horhead[j])]))
+            model.setItem(i, j, item)
+    return model
+
+    #Преобразовать закодированную строку в текст
+def GetTextFromCodes(codeText):
+    codeStr = codeText.split(';')
+    outS = str()
+    for s in codeStr:
+        wordid = s.split(' ')
+        if wordid != ['']:
+            for word in wordid:
+                data = GetData("select word from wordtab"+
+                    " where idGroup='"+word.split('-')[0]+
+                    "' and id='"+word.split('-')[1]+"'")
+                outS=outS+data[0]['word']+' '
+                outS = outS + ';'
+    return outS
--- Program/BotProgram/DataBaseForm.py
+++ Program/BotProgram/DataBaseForm.py
@@ -10,69 +10,31 @@ class DataBaseForm(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi("DataBaseForm.ui", self)
-        self.GetTableModel('wordsgrouptab','id;semantic')
+        self.GetTableModel('wordsgrouptab')
         self.comboBox.currentIndexChanged.connect(self.RefreshTable)
-        self.InitContextMenu()
+
 
     #Вывод модели таблицы
-    def GetTableModel(self, table, header):
-        db = QtSql.QSqlDatabase.addDatabase("QMYSQL")
-        db.setHostName("localhost")
-        db.setDatabaseName("botdb")
-        db.setUserName("root")
-        db.setPassword("4862159357")
-
-        model = QtSql.QSqlTableModel
-        model.set
-        model.setTable(table)
-        model.setEditStrategy(QtSql.QSqlTableModel.OnManualSubmit);
-        data = DataBase.GetTable("SELECT * FROM "+table)
-        horhead=header.split(';')
-        self.label_2.setText('Поиск по ' + horhead[1] + ' :')
-        verhead = []
-        model.setHorizontalHeaderLabels(horhead)
-        for i in range(len(data)):
-            verhead.append(str(''))
-        model.setVerticalHeaderLabels(verhead)
-
-        for i in range(len(verhead)):
-            for j in range(len(horhead)):
-                item = QStandardItemModel()
-                if table == 'dialogtab' and (horhead[j] == 'question' or horhead[j] == 'answer'):
-                    item = QStandardItem(self.GetTextFromCodes(data[i][horhead[j]]))
-                else:
-                    item = QStandardItem(str(data[i][horhead[j]]))
-                model.setItem(i,j,item)
-        self.tableView.setModel(model)
+    def GetTableModel(self, table):
+        self.tableView.setModel(DataBase.GetTableViewModel("select * from " +table))
         self.tableView.resizeRowsToContents()
         self.tableView.resizeColumnsToContents()
 
     #Обновить таблицу
     def RefreshTable(self):
-        ind = self.comboBox.currentIndex()
+        '''ind = self.comboBox.currentIndex()
         if ind == 0:
-            self.GetTableModel('wordsgrouptab', 'id;semantic')
+            self.GetTableModel('wordsgrouptab')
         elif ind == 1:
-            self.GetTableModel('wordtab', 'id;word;idGroup')
+            self.GetTableModel('wordtab')
         elif ind == 2:
-            self.GetTableModel('dialogtab', 'id;question;answer;action')
+            self.GetTableModel('dialogtab')
         elif ind == 3:
-            self.GetTableModel('actiontab', 'id;action;note')
+            self.GetTableModel('actiontab')
+          '''
+        self.GetTableModel(self.comboBox.currentText())
 
-    #Преобразовать закодированную строку в текст
-    def GetTextFromCodes(self, codeText):
-        codeStr = codeText.split(';')
-        outS = str()
-        for s in codeStr:
-            wordid = s.split(' ')
-            if wordid != ['']:
-                for word in wordid:
-                    data = DataBase.GetTable("select word from wordtab"+
-                    " where idGroup='"+word.split('-')[0]+
-                    "' and id='"+word.split('-')[1]+"'")
-                    outS=outS+data[0]['word']+' '
-                outS = outS + ';'
-        return outS
+
 
     def InitContextMenu(self):
         self.tableView.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
